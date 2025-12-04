51079d80ed43693e05de4d2aa37c80e2a1827720
Álvaro Olvera Fernández <aolvera@externas.jccm.es>
2025-12-05 00:49:48 +0100

    PEC2

--- forcontu-vue/src/components/Articles.vue
+++ forcontu-vue/src/components/Articles.vue
@@ -22,7 +22,7 @@ export default {
   },
   created () {
     axios
-      .get(siteDomain + 'jsonapi/node/article', restConfig)
+      .get(siteDomain + '/jsonapi/node/article', restConfig)
       .then(response => (this.articles = response.data))
   },
   components: {
